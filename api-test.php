1dcb30fd6178f7cb5c226fe3288f7561e25db140
santiagokea <43513609+santiagokea@users.noreply.github.com>
2021-10-18 09:57:11 +0200

    Update api-test.php

--- api-test.php
+++ api-test.php
@@ -1,13 +1,24 @@
+
+
+
+
 <?php
 require_once('db.php');
 $user_email = $_POST['user_email'];
 $user_password = $_POST['user_password'];
 
-$q = $_db->prepare("SELECT * FROM users");
+$q = $_db->prepare("SELECT * FROM users 
+                    WHERE user_email='$user_email' 
+                    AND user_password = '$user_password' ");
 $q->execute();
 $rows = $q->fetchAll();
 header('Content-Type: application/json');
 echo json_encode($rows);
+
+
+
+
+
 
 
 
